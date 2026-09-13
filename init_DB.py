9bf83aeb80291245a4129f2fa616ce7621a72ac4
Debraj Dalai <debrajdalai29@gmail.com>
2026-09-13 20:45:26 +0530

    fix

--- init_DB.py
+++ init_DB.py
@@ -2,8 +2,8 @@ from qdrant_client import QdrantClient
 from qdrant_client.models import Filter
 
 client = QdrantClient(
-    url= "https://9b6ff33b-df7c-4328-b2f3-fe5d55ea8955.us-east-2-0.aws.cloud.qdrant.io",
-    api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIiwic3ViamVjdCI6ImFwaS1rZXk6NGQ5MTg0OTAtNzVmNi00NmU3LTlkZDMtYzVjZWExNDc4YzE3In0.nkB3ORVFkVFCJlSID8vbn1JY631GEjZVC9yrd_i548I",
+    url= "",
+    api_key="",
 )
 
 COLLECTION_NAME = "hr_documents"
